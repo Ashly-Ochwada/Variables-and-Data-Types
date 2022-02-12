4d4106839168418bd9d5333dba2634bd2764494f
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-02-12 08:12:14 +0300

    first commit

---
+++ src/main/kotlin/Assign.kt
@@ -0,0 +1,17 @@
+fun main ()
+
+{
+    var name = "Ashly Ochwada"
+    println(name)
+    var age = "19"
+    println(age)
+    var phoneNumber = "0758862979"
+    println(phoneNumber)
+    var weight = "54"
+    println(weight)
+
+    val isKenyan: Boolean = true
+    val isNotKenyan: Boolean = false
+    println(isKenyan)   //output true
+    println(isNotKenyan)   //output false
+}
